64060d4b77cb93d01a14e484dbd24a921cc63453
phb0228 <phb0228@naver.com>
2020-05-08 14:56:49 +0900

    설정파일 수정

--- src/main/java/kr/co/community/user/controller/UserController.java
+++ src/main/java/kr/co/community/user/controller/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("users")
 public class UserController {
 	
 	

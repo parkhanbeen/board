e218b3ad6c27f90f655b7a5cae670273bb3b24fd
parkhanbeen <phb0228@naver.com>
2020-05-12 00:15:16 +0900

    account vo 수정

--- src/main/java/kr/co/community/repository/vo/Account.java
+++ src/main/java/kr/co/community/repository/vo/Account.java
@@ -5,81 +5,11 @@ import java.util.Date;
 public class Account {
 	private int no;
 	private String id;
-	private String pass;
+	private String password;
 	private String name;
-	private String phoneNum;
 	private String email;
-	private String addr;
-	private String addrDetail;	
-	private String postCode;
-	private Date brithday;
 	private String resignation;
 	
-	public int getNo() {
-		return no;
-	}
-	public void setNo(int no) {
-		this.no = no;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPass() {
-		return pass;
-	}
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPhoneNum() {
-		return phoneNum;
-	}
-	public void setPhoneNum(String phoneNum) {
-		this.phoneNum = phoneNum;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getAddr() {
-		return addr;
-	}
-	public void setAddr(String addr) {
-		this.addr = addr;
-	}
-	public String getAddrDetail() {
-		return addrDetail;
-	}
-	public void setAddrDetail(String addrDetail) {
-		this.addrDetail = addrDetail;
-	}
-	public String getPostCode() {
-		return postCode;
-	}
-	public void setPostCode(String postCode) {
-		this.postCode = postCode;
-	}
-	public Date getBrithday() {
-		return brithday;
-	}
-	public void setBrithday(Date brithday) {
-		this.brithday = brithday;
-	}
-	public String getResignation() {
-		return resignation;
-	}
-	public void setResignation(String resignation) {
-		this.resignation = resignation;
-	}	
+
 
 }

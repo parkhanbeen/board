971e2ccb2c7404b6235afe48d1d9b59a36c27183
parkhanbeen <phb0228@naver.com>
2020-05-13 00:11:12 +0900

    lombok dependency 추가

--- src/main/java/kr/co/community/repository/vo/Account.java
+++ src/main/java/kr/co/community/repository/vo/Account.java
@@ -1,7 +1,12 @@
 package kr.co.community.repository.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class Account {
 	private int no;
 	private String id;

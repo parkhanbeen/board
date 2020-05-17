a18f2245ec07d382506adf2339eaad1d3cc883d7
parkhanbeen <phb0228@naver.com>
2020-05-17 21:40:41 +0900

    spring security 비밀번호 암호화 테스트

--- src/main/java/kr/co/community/user/controller/UserController.java
+++ src/main/java/kr/co/community/user/controller/UserController.java
@@ -3,6 +3,7 @@ package kr.co.community.user.controller;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,9 @@ public class UserController {
 	@Autowired
 	private UserService service;
 
+	@Autowired
+	BCryptPasswordEncoder passEncoder;
+
 
 	// login 페이지 이동
 	    @GetMapping("login")
@@ -28,7 +32,9 @@ public class UserController {
 	    
 	    @PostMapping("login") 
 	    public String login(@RequestBody Account account) {
-	    	log.info(account);
+	    	log.info(account.getPassword());
+	    	account.setPassword(passEncoder.encode(account.getPassword()));
+	    	log.info(account.getPassword());
 	    	return "board/list.tiles";
 	    }
 
--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -11,6 +11,30 @@ $(document).on("click", "#login_Btn", () => {
     	swal("비밀번호을 입력해주세요.");		
     	return false;
 	}
-    
+    login.login();
     
 });
+
+let login = {
+	login : function () {
+		console.log($('#id_lg').val());
+		console.log($('#password_lg').val());
+
+
+		let param = {
+			id:$('#id_lg').val(),
+			password:$('#password_lg').val()
+		};
+
+		$.ajax({
+			type: "post",
+			url: "/users/login",
+			data: JSON.stringify(param),
+			dataType: "json",
+			contentType: 'application/json'
+		}).done(function () {
+			alert('성공');
+
+		});
+	}
+};

3988fcbbccf27b5c92309ecd67c5fc8860dc1faf
phb0228 <phb0228@naver.com>
2020-05-21 10:55:40 +0900

    loginVO 수정

--- src/main/java/kr/co/community/repository/vo/Account.java
+++ src/main/java/kr/co/community/repository/vo/Account.java
@@ -3,7 +3,6 @@ package kr.co.community.repository.vo;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
 
 @Getter
 @Setter
@@ -14,6 +13,42 @@ public class Account {
 	private String name;
 	private String email;
 	private String resignation;
+	public int getNo() {
+		return no;
+	}	
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getResignation() {
+		return resignation;
+	}
+	public void setResignation(String resignation) {
+		this.resignation = resignation;
+	}
 	
 
 

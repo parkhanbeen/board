8d1ab9ca32841bbb2ec90554d917a5f7ed1b955d
박한빈 <phb0228@naver.com>
2020-05-25 20:38:51 +0900

    Merge branch 'master' of https://github.com/parkhanbeen/board
    # Conflicts:
    #	src/main/webapp/resources/js/login.js

--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -1,5 +1,8 @@
 let loginExit = false;
 let registerExit = false;
+const alphaDigit= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+const regExp = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/; 
+
 
 $('.message a').click(function(){
 	  $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
@@ -19,11 +22,17 @@ $(document).on("keyup", ".login_txt", () => {
 });
 
 $(document).on("keyup", "#id_reg", () => {
-	const idCh = $('#id_reg').val();
+
+	let idCh = $('#id_reg').val();
 	register.IdCheck(idCh);
 });
 
 $(document).on("click", "#register_Btn", () => {
+	if($('#result_reg_msg').find('p').css('color') === "rgb(253, 71, 43)"){
+		swal($('#result_reg_msg').find('p').text());
+		$('#id_reg').focus();
+		return;
+	}
 	register.validation();
     if(registerExit){
     	return;
@@ -63,13 +72,42 @@ let register = {
 			if(!$('#id_reg').val()){
 				swal("아이디을 입력해주세요.");
 				registerExit = true;
+				$('#id_reg').focus();
 				return;
-			}
+			}	
+			
 		    if(!$('#pass_reg').val()){
 		    	swal("비밀번호을 입력해주세요.");		
 		    	registerExit = true;
+		        $('#pass_reg').focus();
 		    	return;
 			}
+		    if ($('#pass_reg').val().length < 4) {
+		    	swal("비밀번호는 4자리 이상 입력하셔야 합니다.");
+		    	$('#pass_reg').val('');
+		        $('#pass_reg').focus();
+		        registerExit = true;
+		        return;
+		        }
+		    
+		    if ($('#pass_reg').val().indexOf(" ") >= 0) {
+		    	swal("비밀번호에는 공백이 들어가면 안됩니다.");
+		    	$('#pass_reg').val('');
+		        $('#pass_reg').focus();
+		        registerExit = true;
+		        return;
+		        }
+		    
+		    for (i=0; i<$('#pass_reg').val().length; i++) {
+		        if (alphaDigit.indexOf($('#pass_reg').val().substring(i, i+1)) < 0) {
+		        swal("비밀번호는 영문과 숫자의 조합만 사용할 수 있습니다.");
+		    	$('#pass_reg').val('');
+		        $('#pass_reg').focus();
+		        registerExit = true;
+		        return;
+		        } 
+		      }
+		    
 		    if(!$('#name_reg').val()){
 		    	swal("이름을 입력해주세요.");		
 		    	registerExit = true;
@@ -79,25 +117,71 @@ let register = {
 		    	swal("이메일을 입력해주세요.");		
 		    	registerExit = true;
 		    	return;
+			}else{
+				 if(!regExp.test($('#email_reg').val())) { 
+					 swal("이메일 주소가 유효하지 않습니다"); 
+				      registerExit = true;
+				      $('#email_reg').focus(); 
+				      return; 
+				   } 
 			}
 		    registerExit = false;
 		},
 		
 		IdCheck : function(id){
-			var html = '';
+			let html = '';
+			let pTag = ''
+			let special_pattern = /[`~!@#$%^&*|\\\'\";:\/?]/gi; 
+			
 			$('#result_reg_msg').find('p').remove();
+			
+			if ($('#id_reg').val().length < 4 || $('#id_reg').val().length > 15){
+				html = '<p style="color:#fd472b; font-size: 14px; font-weight:bold;">아이디는 4~15자 이내여야 합니다.<p>';
+				$('.result_reg_msg').append(html);
+				$('#result_reg_msg').css('display','block');
+				$('#id_reg').focus();
+				registerExit = true;
+			    return;
+			    }
+			
+			if ($('#id_reg').val().search(/\s/) != -1) {
+			    html = '<p style="color:#fd472b; font-size: 14px; font-weight:bold;">아이디에는 공백이 들어가면 안됩니다.<p>';
+				$('.result_reg_msg').append(html);
+				$('#result_reg_msg').css('display','block');
+				$('#id_reg').focus();
+				registerExit = true;
+			    return;
+			    }
+			
+			if(special_pattern.test($('#id_reg').val()) == true) {
+				html = '<p style="color:#fd472b; font-size: 14px; font-weight:bold;">아이디에 특수문자가 들어가면 안됩니다.<p>';
+				$('.result_reg_msg').append(html);
+				$('#result_reg_msg').css('display','block');
+				$('#id_reg').focus();
+				registerExit = true;
+				return; 
+				}
+			
+			for (i=0; i<$('#id_reg').val().length; i++) {
+			      if (alphaDigit.indexOf($('#id_reg').val().substring(i, i+1)) == -1) {
+			       html = '<p style="color:#fd472b; font-size: 14px; font-weight:bold;">아이디는 영문과 숫자의 조합만 사용할 수 있습니다.<p>';
+					$('.result_reg_msg').append(html);
+					$('#result_reg_msg').css('display','block');
+					$('#id_reg').focus();
+					registerExit = true;
+			      return;
+			      }
+			    }
+			
 			$.ajax({
 				url: "/users/register/" + id ,
 			}).done(function (result) {
 				if (result){
-					$('#not_Available_Id').css('display','none');
-					$('#available_Id').css('display','block');
+					 html = '<p style="color:#fd472b; font-size: 14px; font-weight:bold;">이미 사용중인 아이디 입니다.<p>';
 				}else{
-					$('#available_Id').css('display','none');
-					$('#not_Available_Id').css('display','block');
-//					 html = '<p style="color:##374850; font-size: 14px; font-weight:bold">사용 가능한 아이디 입니다.<p>';
+					 html = '<p style="color:#374850; font-size: 14px; font-weight:bold;">사용 가능한 아이디 입니다.<p>';
 				}
-//				$('.result_reg_msg').append(html);
+				$('.result_reg_msg').append(html);
 				$('#result_reg_msg').css('display','block');
 				});
 		}
@@ -123,8 +207,7 @@ let login = {
 			if (result){
 			location.href="/board/list.do";
 			}else{
-				var html = '<p style="color:#fd472b; font-size: 14px; font-weight:bold">아이디 또는 비밀번호가 일치하지 않습니다 .<p>';
-//				swal("아이디 또는 비밀번호가 일치하지 않습니다 .");
+				let html = '<p style="color:#fd472b; font-size: 14px; font-weight:bold">아이디 또는 비밀번호가 일치하지 않습니다 .<p>';
 				$('.result_lg_msg').append(html);
 				$('#result_lg_msg').css('display','block');
 				return;
